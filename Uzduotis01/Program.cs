78476341f6189cb7ac5b11069c578c1f3019695a
EditaMYT <edita.o@gmail.com>
2023-04-03 16:18:14 +0300

    ND1

---
+++ Uzduotis01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uzduotis01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Labas " + "Edita");
+            Console.WriteLine();
+
+            int amzius = 26;
+            Console.WriteLine("Ivestas amzius: {0}", amzius);
+            Console.WriteLine();
+            
+            int skaicius = 25;
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.WriteLine(skaicius);
+            Console.WriteLine();
+
+            Console.WriteLine($"{skaicius} {skaicius} {skaicius} {skaicius} {skaicius} {skaicius}");
+            Console.WriteLine();
+        }
+    }
+}
