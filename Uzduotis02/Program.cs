a880170785f3be053ec92c0b5c45c9f97f02ba3f
EditaMYT <edita.o@gmail.com>
2023-04-03 16:46:27 +0300

    ND2

---
+++ Uzduotis02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uzduotis02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string startLine = "---------------------";
+            string emptyLine = "|                   |";
+            string name = "Martynas";
+            Console.WriteLine(startLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine("|    Mano vardas    |");
+            Console.WriteLine("|      " + name + "     |");
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(startLine);
+            Console.WriteLine();
+        }
+    }
+}
