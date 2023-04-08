763c2c01dd9b1dbcca7aac1b6e96d7a284d3ea25
EditaMYT <edita.o@gmail.com>
2023-04-08 19:00:07 +0300

    ND4

---
+++ Uzduotis04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uzduotis04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Susikurkite reikamus kintamuosius uzduociai atlikti. I ekrana isvekite: 
+            Automobilio {marke} {modelis} ({metai} m.) rida - {rida} km.
+            Tarp {} nurodyti reikiami kintamieji, siose vietose ju reiksmes turi buti istatytos.
+            */
+
+            string marke = "Audi";
+            string modelis = "A4";
+            int metai = 2020;
+            int rida = 20000;
+            
+            Console.WriteLine($"Automobilio {marke} {modelis} ({metai} m.) rida - {rida} km.");
+        }
+    }
+}
