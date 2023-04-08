46be86cd092b3ee9859dddb8cb95de14442f67a4
EditaMYT <edita.o@gmail.com>
2023-04-08 16:46:13 +0300

    ND3

---
+++ Uzduotis03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uzduotis03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Isveskite i ekrana norimo dydzio tusciaviduri kvadrata is zvaigzducių;
+             ****
+             *   *
+             *   *
+             ****
+             */
+
+            Console.WriteLine("*******");
+            Console.WriteLine("*     *");
+            Console.WriteLine("*     *"); 
+            Console.WriteLine("*******");
+        }
+    }
+}
