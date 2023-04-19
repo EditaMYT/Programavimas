90f775153a5319a7da163f76280d91e33a47898d
EditaMYT <edita.o@gmail.com>
2023-04-19 21:43:30 +0300

    ND28

---
+++ Uzduotis28/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uzduotis28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pastebejimas();
+        }
+
+        static private void Pastebejimas()
+        {
+            Console.WriteLine("Atejo pavasaris, bet dar salta!");
+            Console.WriteLine();
+        }
+        
+    }
+}
