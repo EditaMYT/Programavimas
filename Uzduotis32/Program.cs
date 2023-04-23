dde97a85d6a4fa6afe92af0bc9968fc7ca4694e9
EditaMYT <edita.o@gmail.com>
2023-04-23 12:20:56 +0300

    ND32

---
+++ Uzduotis32/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uzduotis32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1. Paprašyti įvesti vartotojo vardą.
+            2. Sukurti string tipo metodą, kuris gražina frazę pvz „Sveiki, Mantas“.
+            3. Išvesti šią frazę į konsolę.
+            */
+
+            Console.WriteLine("Iveskite savo varda:");
+            Console.ReadLine();
+            Console.WriteLine();
+            string vardas = "Edita";
+            Pasisveikinimas(vardas);
+        }
+
+        private static void Pasisveikinimas(string vardas)
+        {
+            Console.WriteLine($"Sveiki, {vardas}");
+            Console.WriteLine();
+        }
+    }
+}
